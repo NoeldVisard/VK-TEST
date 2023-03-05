94114b14b03a811d5cb17516fd8bbf2e7d265950
rodion.karimov <rodion.karimov@tattelecom.digital>
2023-03-05 20:54:09 +0300

    Passing of all peaks

--- src/Controller/LabyrinthController.php
+++ src/Controller/LabyrinthController.php
@@ -25,21 +25,61 @@ class LabyrinthController extends AbstractController
         $data = $request->request->all();
 
         $lbr = $labyrinthService->parseLabyrinth($data);
-        $start = $data["start"];
-        $finish = $data["finish"];
+        $start = explode(" ", $data["start"]); // ["0", "1"]
+        $finish = explode(" ", $data["finish"]); // ["0", "9"]
+//        $lbr = [
+//            [0, 1, 0, 0, 0, 2, 3, 2, 0, 1],
+//            [1, 1, 2, 1, 0, 1, 0, 1, 1, 1],
+//            [1, 0, 0, 3, 4, 5, 0, 0, 0, 1],
+//            [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+//            [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//        ]; // test labyrinth array
 
-        $lbr = [
-            [0, 1, 0, 0, 0, 2, 3, 2, 0, 1],
-            [1, 1, 2, 1, 0, 1, 0, 1, 1, 1],
-            [1, 0, 0, 3, 4, 5, 0, 0, 0, 1],
-            [1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ];
+        $moves = [[0, 1], [0, -1], [1, 0], [-1, 0]]; // возможные направления движения
+        $visited = array_fill(0, count($lbr), array_fill(0, count($lbr[0]), false)); // матрица посещённых вершин
+        $visited[$start[0]][$start[1]] = true;
+        $cost = array_fill(0, count($lbr), array_fill(0, count($lbr[0]), INF)); // матрица стоимости пути из стартовой вершины
+        $cost[$start[0]][$start[1]] = $lbr[$start[0]][$start[1]];
+        $finish = false;
+        $currentPoint = [$start[0], $start[1]];
+        $currentCost = $lbr[$start[0]][$start[1]];
+
+        while (!$finish) {
+            foreach ($moves as $move) {
+                if (isset($lbr[$currentPoint[0] + $move[0]][$currentPoint[1] + $move[1]])) {
+                    $nextPoint = [$currentPoint[0] + $move[0], $currentPoint[1] + $move[1]];
+                    //  если нашли финиш
+                    if ($nextPoint[0] == $finish[0] & $nextPoint[1] == $finish[1]) {
+                        $finish = true;
+                        break;
+                    }
+                    
+                    if ($lbr[$nextPoint[0]][$nextPoint[1]] == 0) // если стена
+                        continue;
+
+                    // если нет, тогда ставим как пройденную вершину
+                    $visited[$nextPoint[0]][$nextPoint[1]] = true;
+                    $currentCost += $lbr[$nextPoint[0]][$nextPoint[1]];
+                    // если новое найденное расстояние до вершины меньше, чем раньше найденное
+                    if ($cost[$nextPoint[0]][$nextPoint[1]] > $currentCost) {
+                        $cost[$nextPoint[0]][$nextPoint[1]] = $currentCost;
+                    }
+                    
+                    // рекурсивный вызов
+                }
+            }
+        }
+
         return new Response('Путь');
+    }
+
+    private function fillOutCostMatrix()
+    {
+
     }
 }
